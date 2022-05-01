abbdce5809db1c5884a2e2a2805a4edb9bcec4c5
FilstonMbambi <mbambifilston038@gmail.com>
2022-05-01 20:56:48 +0200

    enabled calculation of two numbers

--- app.js
+++ app.js
@@ -7,9 +7,7 @@ const allClear = document.querySelector("#clear");
 const del = document.querySelector("#del");
 const numbers = document.querySelectorAll("#btn-num");
 const operations = document.querySelectorAll("#btn-ops");
-
-// equation variable
-let equation = "";
+const equals = document.querySelector("#equals");
 
 // Add event handlers on elements:
 
@@ -30,87 +28,50 @@ numbers.forEach((number) => {
         topDisplay.textContent += number.textContent;
     })
 })
-// equation += topDisplay.textContent;
-// console.log(equation);
 
 // Operations
 operations.forEach((operation) => {
     operation.addEventListener("click", () => {
-        topDisplay.textContent += ` ${operation.textContent} `;
+        topDisplay.textContent += `${operation.textContent}`;
     })
 })
-equation += topDisplay.textContent;
-console.log(equation)
+
+// Equals
+equals.addEventListener("click", () => {
+    bottomDisplay.textContent = handleEquation(topDisplay.textContent);
+})
 
 // Perform calculations
-function calculate(firstNumber, operator, secondNumber) {
+function calculate(firstNum, operator, secondNum) {
+    firstNum = Number(firstNum);
+	secondNum = Number(secondNum);
 
-	firstNumber = Number(firstNumber);
-	secondNumber = Number(secondNumber);
-
-    if (operator === 'plus' || operator === '+') return firstNumber + secondNumber;
-    if (operator === 'minus' || operator === '-') return firstNumber - secondNumber;
-    if (operator === 'multiply' || operator === 'x') return firstNumber * secondNumber;
-    if (operator === 'divide' || operator === '/') return firstNumber / secondNumber;
-    if (operator === 'remainder' || operator === '%') return firstNumber % secondNumber;
+    if (operator === '+') return firstNum + secondNum;
+    if (operator === '-') return firstNum - secondNum;
+    if (operator === 'x') return firstNum * secondNum;
+    if (operator === '÷') return firstNum / secondNum;
 }
 
-// function handleEquation(equation) {
+function handleEquation(equation) {
+   const operators = ["÷", "x", "+", "-"];
+   equation = equation.split("");
+   let firstNum;
+   let secondNum;
+   let operator;
+   let operatorIndex;
+   let result;
 
-// 	equation = equation.split(" ");
-// 	const operators = ['/', 'x', '%', '+', '-'];
-// 	let firstNumber;
-// 	let secondNumber;
-// 	let operator;
-// 	let operatorIndex;
-// 	let result;
-
-// 	/*  
-// 		1. Perform calculations as per BODMAS Method
-// 		2. For that use operators array
-// 		3. after calculation of 1st numbers replace them with result
-// 		4. use splice method
-
-// 	*/
-// 	for (var i = 0; i < operators.length; i++) {
-// 		while (equation.includes(operators[i])) {
-// 			operatorIndex = equation.findIndex(item => item === operators[i]);
-// 			firstNumber = equation[operatorIndex-1];
-// 			operator = equation[operatorIndex];
-// 			secondNumber = equation[operatorIndex+1];
-// 			result = calculate(firstNumber, operator, secondNumber);
-// 			equation.splice(operatorIndex - 1, 3, result);
-// 		}
-// 	}
-
-// 	return result;
-// }
-
-// // Event Listener for keyboard button press
-// document.addEventListener('keydown', (event) => {
-	
-// 	let getOperators = {
-// 		'/': 'divide',
-// 		'x': 'multiply',
-// 		'*': 'multiply',
-// 		'%': 'remainder',
-// 		'+': 'plus',
-// 		'-': 'minus'
-// 	}
-
-// 	if(!isNaN(event.key) && event.key !== ' '){
-// 		document.getElementById(`digit-${event.key}`).click();
-// 	}
-// 	if (['/', 'x', '+', '-', '*', '%'].includes(event.key)) {
-// 		document.getElementById(getOperators[event.key]).click();
-// 	}
-// 	if (event.key === 'Backspace' || event.key ==='c' || event.key === 'C') {
-// 		document.getElementById('clear').click();	
-// 	}
-// 	if (event.key === '=' || event.key === 'Enter') {
-// 		document.getElementById('equals').click();	
-// 	}
-// 	if (event.key === '.') {
-// 		document.getElementById('decimal').click();	
-// 	}
-// });
+   for (let i = 0; i < equation.length; i++) {
+       for (let j = 0; j < operators.length; j++) {
+            if (equation[i] === operators[j]) {
+                operator = operators[j];
+                operatorIndex = equation.indexOf(operator);
+                firstNum = equation[operatorIndex - 1];
+                secondNum = equation[operatorIndex + 1];
+                result = calculate(firstNum, operator, secondNum);
+                equation.splice(operatorIndex - 1, 3, result);
+            }
+       }
+   }
+   return result;
+}
